247083a70b05f8f0ba31a432d201a02f4b017dd0
happy7yong <happy7yong83@gmail.com>
2026-01-03 17:14:19 +0900

    docs : 코딩테스트 js 파일 작성 - 성문열고닫기

---
+++ Algorithm/algorithm-kyo/1. Stack_Queue_Hash/open-and-close-the-gates.js
@@ -0,0 +1,20 @@
+function solution(gate) {
+    const stack = [];
+
+    for (let char of gate){
+        if(char === "<"){
+            stack.push(char);
+        }else{
+            if(stack.length === 0){
+                return false
+            }
+            stack.pop();
+        }
+    }
+
+    return stack.length === 0;
+}
+console.log(solution("<<>>"))
+console.log(solution("<<>>"))
+console.log(solution(">><<"))
+console.log(solution("<<<>"))
